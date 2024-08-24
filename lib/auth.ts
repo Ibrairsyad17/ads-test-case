209fc6614f964f7760fe2c775c5f1d75142d72a8
Ibrahim Irsad <ibrairsyad1712@gmail.com>
2024-08-24 16:44:11 +0700

    feat (authenticate): sign in logic and protected routes using middleware with mysql

---
+++ lib/auth.ts
@@ -0,0 +1,52 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "@/prisma/client";
+import { compare } from "bcrypt";
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.username || !credentials?.password) {
+          return null;
+        }
+
+        const existingUser = await prisma.user.findUnique({
+          where: { username: credentials?.username },
+        });
+
+        if (!existingUser) {
+          return null;
+        }
+
+        const passwordMatch = await compare(
+          credentials.password,
+          existingUser.password,
+        );
+        if (!passwordMatch) {
+          return null;
+        }
+
+        return {
+          id: existingUser.id,
+          username: existingUser.username,
+          email: existingUser.email,
+        };
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/login",
+  },
+};
